8b27056460f8f9a8591b82e1d8dfd6dbf6dbef55
keshav1245 <tangri57@gmail.com>
2020-07-12 13:34:34 +0530

    Assignment 1

--- 10. Assignment 1/assignment1-starter-code/app.js
+++ 10. Assignment 1/assignment1-starter-code/app.js
@@ -7,17 +7,38 @@
   function LunchCheckController($scope){
     $scope.lunchitems = "";
     $scope.message = "";
+    $scope.styleMessage = "";
+    $scope.styleBorder = "";
 
     $scope.check = function(){
-      var len = $scope.lunchitems.length;
+      var len = 0;
+      items = $scope.lunchitems.split(',');
+
+      for (var i = 0; i < items.length; i++) {
+        if (items[i].trim() != "") {
+          len += 1; //Checking for the option case 3 : item1 ,item2  , , item and item1, item2,,item3
+        }
+      }
+
       if (len == 0){
         $scope.message = "Please enter data first";
+        $scope.styleMessage = {"color" : "red"}; //optional case 1
+        $scope.styleBorder = {"border" : "1px solid red"}; //optional case 2
       }else{
 
-        if ($scope.lunchitems.split(',').length <= 3){
+        if (len <= 3){
+          $scope.styleMessage = {
+                                    "color" : "green", // optional case 1
+
+                                };
+          $scope.styleBorder = {"border" : "1px solid green"}; // optional case 2
           $scope.message = "Enjoy!";
+
         }else{
+          $scope.styleMessage = {"color" : "green"}; // optional case 1
+          $scope.styleBorder = {"border" : "1px solid green"}; // optional case 2
           $scope.message = "Too Much";
+
         }
 
       }
